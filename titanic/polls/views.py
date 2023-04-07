f386e1fcfceb80e54f76a72b06c17d7ddc07d417
lea <lea.sagnier@gmail.com>
2023-04-07 17:41:49 +0200

    derniere modif

--- titanic/polls/views.py
+++ titanic/polls/views.py
@@ -8,13 +8,13 @@ from django import forms
 import requests
 
 class ContactForm(forms.Form):
-    age = forms.CharField(label='Age', max_length=100, required = True)
-    EMBARQUEMENT_CHOICES = [('C', 'Cherbourg'), ('Q', 'Queenstown'), ('S', 'Southampton')]
-    embarquement = forms.CharField(label='Lieu d\'embarquement', widget=forms.Select(choices=EMBARQUEMENT_CHOICES), max_length=100, required = True)
+    age = forms.CharField(label='Age *', max_length=100, required = True)
     GENDER_CHOICES = [(0, 'Homme'), (1, 'Femme')]
-    sex = forms.CharField(label='Sex',  max_length=100, widget=forms.Select(choices= GENDER_CHOICES), required = True)
-    CLASSE_CHOICES = [(0, 'Première classe'), (1, 'Second class'), (2, 'Troisieme classe')]
-    classe = forms.CharField(label='Classe', max_length=100,widget=forms.Select(choices=CLASSE_CHOICES), required = True)
+    sexe = forms.CharField(label='Sexe *',  max_length=100, widget=forms.Select(choices= GENDER_CHOICES), required = True)
+    EMBARQUEMENT_CHOICES = [('0', 'Southampton'), ('1', 'Cherbourg'), ('2', 'Queenstown')]
+    embarquement = forms.CharField(label='Lieu d\'embarquement *', widget=forms.Select(choices=EMBARQUEMENT_CHOICES), max_length=100, required = True)
+    CLASSE_CHOICES = [(1, 'Première classe'), (2, 'Second class'), (3, 'Troisieme classe')]
+    classe = forms.CharField(label='Classe * ', max_length=100,widget=forms.Select(choices=CLASSE_CHOICES), required = True)
     # widget=forms.Textarea)
 
 def index(request):
@@ -24,14 +24,15 @@ def index(request):
             # Récupérer les données du formulaire
             age = form.cleaned_data['age']
             embarquement = form.cleaned_data['embarquement']
-            sex = form.cleaned_data['sex']
+            sexe = form.cleaned_data['sex']
             classe = form.cleaned_data['class']
 
             # Envoyer les données à l'API FastAPI
-            data = {'age': age, 'embarquement': embarquement, 'sex': sex, 'classe': classe  }
-            response = requests.post('http://localhost:8000/predict', json=data)
+            data = {'age': age, 'embarquement': embarquement, 'sexe': sexe, 'classe': classe  }
+            response = requests.post('http:/127.0.0.1:8000/main', json=data)
 
             # Vérifier la réponse de l'API
+
     else:
         form = ContactForm()
 
