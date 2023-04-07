bd8b07d1edcc4da1bf34d7a07d3d81a0010b7b11
Lauraquat <lauraquat@gmail.com>
2023-04-07 17:43:47 +0200

    modif jupyter

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from typing import Union
 from fastapi import FastAPI
 import pandas as pd
 import xgboost as xgb
-import numpy as np
 
 # On charge le modèle
 model2 = xgb.XGBClassifier()
@@ -11,13 +10,21 @@ model2.load_model("modelTitanic.json")
 # On créé l'application FastAPI
 app = FastAPI()
 
+
+if( Pclass == 1):
+    Fare == 84.15
+elif( Pclass == 2):
+    Fare == 20.66
+elif( Pclass == 3):
+    Fare == 13.67
+
+
 # On créé une route pour faire des prédictions
 @app.get("/predict")
 def predict():
     # On met les données dans un tableau NumPy
-    data = pd.DataFrame([[36, 2, 1, 2, 17]], columns = ["Age", "Embarked", "Sex", "Pclass", "Fare"])
-    # On créé un DMatrix à partir de vos données 
-    dmatrix = xgb.DMatrix(data)
+    data = pd.DataFrame([[Age, Embarked, Sex, Pclass, Fare]], columns = ["Age", "Embarked", "Sex", "Pclass", "Fare"])
+   
     # On utilise le DMatrix pour faire la prédiction
     prediction = model2.predict_proba(data)
     print(prediction) 
